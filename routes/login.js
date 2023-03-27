421c3f4a4b887628cb5dea4ec1da2a39f3efd2c3
Your Name <fred.postles@gmail.com>
2023-03-27 11:36:12 +0100

    removed chalk

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,6 @@ const { checkCreds, addToken } = require("../mysql/queries");
 const { getUniqueId } = require("../utils");
 const router = express.Router();
 const sha256 = require("sha256");
-const chalk = require("chalk");
 
 router.post("/", async (req, res) => {
   let { email, password } = req.body;
